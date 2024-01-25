9ab4f1c97c64b619bdaf9023cdba9fe3fa096cdd
jasobo <jan2505sobotta@gmail.com>
2024-01-25 11:35:58 +0100

    Aufgabe Hello World

--- src/main/java/de/ausbildung/HelloWorldController.java
+++ src/main/java/de/ausbildung/HelloWorldController.java
@@ -1,13 +1,24 @@
 package de.ausbildung;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld(){
-        return "Hello world!";
+        return "Hello world";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloWorldPathVariable(@PathVariable String name){
+        return "Hello World " +name;
+    }
+
+    @GetMapping("/helloworld")
+    @ResponseBody
+    public String helloWorldQueryParam(@RequestParam String name){
+        return "Hello World "+name;
     }
 }
